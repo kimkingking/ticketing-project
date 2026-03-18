1a4cd9cab73c87b4d339e78565373b4c7d6a6b4f
kimkingking <rlaqhgp00@gmail.com>
2026-03-18 16:24:51 +0900

    bohye

--- ticketing-was/database.py
+++ ticketing-was/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import redis
 
 # was_user 계정 사용!
-DB_URL = "mysql+pymysql://was_user:1234@mysql-service:3306/ticket?charset=utf8mb4"
+DB_URL = "mysql+pymysql://was_user:1234@mysql:3306/ticket?charset=utf8mb4"
 engine = create_engine(DB_URL, echo=True)
 
 rd = redis.Redis(host='redis-service', port=6379, db=0, decode_responses=True)
